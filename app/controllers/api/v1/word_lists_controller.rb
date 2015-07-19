0c78f3ffc4bdf50cc37351fe468b9727b0e13622
Kevin Qi <iqnivek@gmail.com>
2015-07-19 13:53:44 -0400

    Add create method on wordlist

--- app/controllers/api/v1/word_lists_controller.rb
+++ app/controllers/api/v1/word_lists_controller.rb
@@ -1,9 +1,26 @@
 class Api::V1::WordListsController < ApplicationController
   def create
+    list = WordList.new(name: params[:name])
+
+    word_list_params[:words].each do |word|
+      list.words.build(word: word)
+    end
+
+    if list.save
+      render json: list
+    else
+      # TODO
+    end
   end
 
   def show
     list = WordList.find(params[:id])
     render json: list
   end
+
+  private
+
+    def word_list_params
+      params.require(:word_list).permit(:name, :words)
+    end
 end
--- app/models/word_list.rb
+++ app/models/word_list.rb
@@ -1,4 +1,7 @@
 class WordList < ActiveRecord::Base
   has_many :words
   has_many :games
+
+  # TODO add not null db constraint
+  validates :name, presence: true
 end
